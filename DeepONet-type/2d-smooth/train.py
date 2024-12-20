5c3c256c820732eb40cdf88a8d61c36956d48b43
yolo951 <dt20@mails.tsinghua.edu.cn>
2024-12-20 16:13:36 +0800

    Code Refactoring

--- DeepONet-type/2d-smooth/train.py
+++ DeepONet-type/2d-smooth/train.py
@@ -53,7 +53,7 @@ def b(x,y):
         a = 0
     return a
 
-N = 16
+N = 32
 ntrain = 1000  
 ntest = 200
 ntotal = ntrain + ntest
@@ -61,27 +61,24 @@ alpha = 1 #interface jump
 beta = 0
 eps = 1.0
 
-epochs = 10000
+epochs = 5000
 learning_rate = 0.001
-batch_size = 64
-step_size = 2000
-gamma = 0.5
-model = DNN([N**2,512,128,128,512,4*N**2]).to(device)
+batch_size = 32
+step_size = 1500
+gamma = 0.6
+model = DNN([(N+1)**2,512,256,128,128,256,512,4*N**2]).to(device)
 # model = encoder_decoder().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 
-f = np.load(r'DeepONet-type\2d-smooth\f.npy')
-f_total = np.load(r'DeepONet-type\2d-smooth\matrixf.npy')
 # U_total = np.load(r'DeepONet-type\2d-smooth\matrixU.npy')
-index_of_u = np.load(r'DeepONet-type\2d-smooth\index_of_u.npy')
-val_of_u = np.load(r'DeepONet-type\2d-smooth\val_of_u.npy')
-B_total = np.load(r'DeepONet-type\2d-smooth\vectorB.npy')
-C_total = np.load(r'DeepONet-type\2d-smooth\vectorC.npy')
-up_total = np.load(r'DeepONet-type\2d-smooth\matrixup.npy')
+data = np.load(r"DeepONet-type\2d-smooth\saved_data\data.npz")
+f_total = data['f_total']
+f = f_total.reshape((ntotal, N+1, N+1))
+B_total, C_total, up_total, index_of_u, val_of_u, ut_fine = data['B_total'], data['C_total'], data['up_total'], data['index'], data['val'], data['u_test_fine']
 f_train = torch.tensor(f_total[0:ntrain], dtype=torch.float32).to(device)
-index_of_u_train = torch.LongTensor(index_of_u[0:ntrain]).to(device)
-val_of_u_train = torch.tensor(val_of_u[0:ntrain], dtype=torch.float32).to(device)
+index_of_u = torch.LongTensor(index_of_u).to(device)
+val_of_u = torch.tensor(val_of_u, dtype=torch.float32).to(device)
 B_train = torch.tensor(B_total[0:ntrain], dtype=torch.float32).to(device)
 C_train = torch.tensor(C_total[0:ntrain], dtype=torch.float32).to(device)
 up_train = up_total[0:ntrain]
@@ -89,7 +86,7 @@ up_train = up_total[0:ntrain]
 f_test = torch.tensor(f_total[ntrain:ntotal], dtype=torch.float32).to(device)
 up_test = torch.tensor(up_total[ntrain:ntotal], dtype=torch.float32).to(device)
 C_test = torch.tensor(C_total[ntrain:ntotal], dtype=torch.float32).to(device)
-train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(f_train, index_of_u_train, val_of_u_train, B_train, C_train), batch_size=batch_size, shuffle=True)
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(f_train, B_train, C_train), batch_size=batch_size, shuffle=True)
 mseloss = torch.nn.MSELoss()
 
 index_jump = []
@@ -117,9 +114,11 @@ rel_l2_history = []
 for i in range(epochs):
     model.train()
     train_mse = 0
-    for fb, index_u, val_u, Bb, Cb in train_loader:
+    for fb, Bb, Cb in train_loader:
         optimizer.zero_grad()
         Cb_pred = model(fb)
+        val_u = val_of_u.unsqueeze(0).repeat(fb.shape[0], 1, 1)
+        index_u = index_of_u.unsqueeze(0).repeat(fb.shape[0], 1, 1)
         U_jump = torch.index_select(val_u, 1, index_jump)
         B_jump = torch.index_select(Bb, 1, index_jump)
         U_continuous = torch.index_select(val_u, 1, index_continuous)
@@ -130,7 +129,7 @@ for i in range(epochs):
         loss_jump = mseloss(torch.einsum('bri, bri->br', U_jump, torch.index_select(new_Cb_pred, 1, index_jump)), B_jump)
         loss_continuous = mseloss(torch.einsum('bri, bri->br', U_continuous, torch.index_select(new_Cb_pred, 1, index_continuous)), B_continuous)
         loss_boundary = mseloss(torch.einsum('bri, bri->br', U_boundary, torch.index_select(new_Cb_pred, 1, index_boundary)), B_boundary)
-        loss = 500*loss_jump + 10000*loss_continuous +1000*loss_boundary
+        loss = 50.0*loss_jump + 50.0*loss_continuous +100.0*loss_boundary
         loss.backward()                  
         optimizer.step()  
         train_mse += loss.item()
@@ -145,14 +144,14 @@ for i in range(epochs):
         rel_l_infty = torch.linalg.norm(u_pred.flatten() - u.flatten(), ord=torch.inf) / torch.linalg.norm(u.flatten(), ord=torch.inf)
         rel_l2_history.append(rel_l2.item())
         print('epoch',i,': loss ',train_mse, 'rel_l2 ',rel_l2.item(), 'rel_l_infty ',rel_l_infty.item())
-np.save(r'DeepONet-type\2d-smooth\loss_history.npy', loss_history)
-np.save(r'DeepONet-type\2d-smooth\rel_l2_history.npy', rel_l2_history)
-torch.save(model.state_dict(), r'DeepONet-type\2d-smooth\model_state.pt')
+np.save(r'DeepONet-type\2d-smooth\saved_data\loss_history.npy', loss_history)
+np.save(r'DeepONet-type\2d-smooth\saved_data\rel_l2_history.npy', rel_l2_history)
+torch.save(model.state_dict(), r'DeepONet-type\2d-smooth\saved_data\model_state.pt')
 
 C_pred = model(f_train).detach().cpu().reshape(f_train.shape[0], -1)
 up_pred = np.zeros((ntrain,N,N))
 for k in range(ntrain):
-    interpolate_f_2d = interpolate.RegularGridInterpolator((np.linspace(0, 1, N),np.linspace(0, 1, N)), f[k])
+    interpolate_f_2d = interpolate.RegularGridInterpolator((np.linspace(0, 1, N+1),np.linspace(0, 1, N+1)), f[k])
     F = lambda x, y : interpolate_f_2d((x,y))
     C = C_pred[k]    
     for i in range(0,N):
@@ -173,11 +172,10 @@ print('relative l2 error on train data: ',rel_l2)
 C_pred = model(f_test).detach().cpu().reshape(f_test.shape[0], -1)
 up_pred = np.zeros((ntest,N,N))
 
-M = 8 # M-times test-resolution
+M = 4 # M-times test-resolution
 up_refine = np.zeros((ntest, M*N+1,M*N+1))
-ut_refine = np.zeros((ntest, M*N+1,M*N+1))
 for k in range(ntest):
-    interpolate_f_2d = interpolate.RegularGridInterpolator((np.linspace(0, 1, N),np.linspace(0, 1, N)), f[ntrain+k])
+    interpolate_f_2d = interpolate.RegularGridInterpolator((np.linspace(0, 1, N+1),np.linspace(0, 1, N+1)), f[ntrain+k])
     F = lambda x, y : interpolate_f_2d((x,y))
     C = C_pred[k]    
     for i in range(0,N):
@@ -206,26 +204,17 @@ for k in range(ntest):
             c2p = Cp[4*N*j+4*i+1]
             c3p = Cp[4*N*j+4*i+2]
             c4p = Cp[4*N*j+4*i+3]
-            c1t = Ct[4*N*j+4*i]
-            c2t = Ct[4*N*j+4*i+1]
-            c3t = Ct[4*N*j+4*i+2]
-            c4t = Ct[4*N*j+4*i+3]
             for ki in range(0,M):
                 for kj in range(0,M):
                     xhi = -1/(2*N) + ki*hh
                     xhj = -1/(2*N) + kj*hh
                     up_refine[k, j*M+kj,i*M+ki] = f0/c0 + c1p*np.exp(mu0*xhi) + c2p*np.exp(-mu0*xhi) + c3p*np.exp(mu0*xhj) + c4p*np.exp(-mu0*xhj)
-                    ut_refine[k, j*M+kj,i*M+ki] = f0/c0 + c1t*np.exp(mu0*xhi) + c2t*np.exp(-mu0*xhi) + c3t*np.exp(mu0*xhj) + c4t*np.exp(-mu0*xhj)
     for l in range(0,M*N+1):
         s = l*hh
         up_refine[k, 0,l] = b(s,0)
         up_refine[k, M*N,l] = b(s,1)
         up_refine[k, l,0] = b(0,s)
         up_refine[k, l,M*N] = b(1,s)
-        ut_refine[k, 0,l] = b(s,0)
-        ut_refine[k, M*N,l] = b(s,1)
-        ut_refine[k, l,0] = b(0,s)
-        ut_refine[k, l,M*N] = b(1,s)
 
 k = random.randrange(ntest) # select a random sample from the test dataset to show the error between the true value and the predicted value
 up_test = np.array(up_test.cpu())
@@ -242,14 +231,15 @@ ax.title.set_text('reference solution u(x,y)')
 rel_l2 = np.linalg.norm(up_pred - up_test) / np.linalg.norm(up_test)
 print('relative l2 error on test data: ',rel_l2)
 
-rel_l2 = np.linalg.norm(up_refine - ut_refine) / np.linalg.norm(ut_refine)
-rel_l_infty = np.linalg.norm((up_refine - ut_refine).flatten(), ord=np.inf) / np.linalg.norm(ut_refine.flatten(), ord=np.inf)
+rel_l2 = np.linalg.norm(up_refine - ut_fine) / np.linalg.norm(ut_fine)
+rel_l_infty = np.linalg.norm((up_refine - ut_fine).flatten(), ord=np.inf) / np.linalg.norm(ut_fine.flatten(), ord=np.inf)
 print('relative l2 error on test data (M-times test-resolution): ',rel_l2)
 print('relative l_infty error on test data (M-times test-resolution): ',rel_l_infty)
 
 xh = np.linspace(0,1,N*M+1)
 yh = np.linspace(0,1,N*M+1)
-xxh,yyh = np.meshgrid(xh,yh)
+xxh1, yyh1 = np.meshgrid(xh[:N*M//2], yh)
+xxh2, yyh2 = np.meshgrid(xh[N*M//2+1:], yh)
 
 fig = plt.figure(figsize=(12, 3.5))
 # [left, bottom, width, height]
@@ -259,15 +249,19 @@ ax_cb = fig.add_axes([0.60, 0.1, 0.01, 0.8])
 ax2 = fig.add_axes([0.68, 0.1, 0.25, 0.8])
 ax_cb2 = fig.add_axes([0.94, 0.1, 0.01, 0.8])
 
-vmin = min(up_refine[k].min(), ut_refine[k].min())
-vmax = max(up_refine[k].max(), ut_refine[k].max())
+vmin = min(up_refine[k].min(), ut_fine[k].min())
+vmax = max(up_refine[k].max(), ut_fine[k].max())
 levels = np.linspace(vmin, vmax, 100)
-cs0 = ax0.contourf(xxh, yyh, up_refine[k], levels=levels, cmap='RdYlBu_r')
-cs1 = ax1.contourf(xxh, yyh, ut_refine[k], levels=levels, cmap='RdYlBu_r')
+cs0 = ax0.contourf(xxh1, yyh1, up_refine[k, :, :N*M//2], levels=levels, cmap='RdYlBu_r')
+ax0.contourf(xxh2, yyh2, up_refine[k, :, N*M//2+1:], levels=levels, cmap='RdYlBu_r')
+cs1 = ax1.contourf(xxh1, yyh1, ut_fine[k, :, :N*M//2], levels=levels, cmap='RdYlBu_r')
+ax1.contourf(xxh2, yyh2, ut_fine[k, :, N*M//2+1:], levels=levels, cmap='RdYlBu_r')
 cbar = fig.colorbar(cs0, cax=ax_cb, format='%.3f')
-error = np.abs(up_refine[k]-ut_refine[k])
+error = np.abs(up_refine[k]-ut_fine[k])
+error = np.hstack((error[:, :N*M//2], error[:, N*M//2+1:]))
 levels_error = np.linspace(error.min(), error.max(), 100)
-cs2 = ax2.contourf(xxh, yyh, error, levels=levels_error, cmap='RdYlBu_r')
+cs2 = ax2.contourf(xxh1, yyh1, error[:, :N*M//2], levels=levels_error, cmap='RdYlBu_r')
+ax2.contourf(xxh2, yyh2, error[:, N*M//2:], levels=levels_error, cmap='RdYlBu_r')
 cbar2 = fig.colorbar(cs2, cax=ax_cb2, format='%.3f')
 
 ax0.set_title('Refinement prediction', fontsize=14)
@@ -276,7 +270,7 @@ ax2.set_title('Point-wise error', fontsize=14)
 
 for ax in [ax0, ax1, ax2]:
     ax.set_aspect('equal')
-plt.savefig(r'DeepONet-type\2d-singular\2d_singular_compare.png')
+# plt.savefig(r'DeepONet-type\2d-smooth\saved_data\2d_smooth_compare.png')
 
 plt.figure()
 plt.plot(loss_history)
@@ -284,7 +278,7 @@ plt.xlabel('epochs')
 plt.ylabel('loss')
 # plt.ylim(1e-5, 1e+2)
 plt.yscale("log")
-plt.savefig(r'DeepONet-type\2d-smooth\2d_smooth_loss.png')
+plt.savefig(r'DeepONet-type\2d-smooth\saved_data\2d_smooth_loss.png')
 # plt.show()
 
 plt.figure()
@@ -293,7 +287,7 @@ plt.xlabel('epochs')
 plt.ylabel('relative l2 error')
 # plt.ylim(1e-3, 1e+2)
 plt.yscale("log")
-plt.savefig(r'DeepONet-type\2d-smooth\2d_smooth_l2.png')
+plt.savefig(r'DeepONet-type\2d-smooth\saved_data\2d_smooth_l2.png')
 plt.show()
 
 
