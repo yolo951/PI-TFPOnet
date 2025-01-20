b851e4adc845077598ad17a989a59d147c962936
yolo951 <dt20@mails.tsinghua.edu.cn>
2025-01-20 20:23:37 +0800

    Revert to the correct version

--- DeepONet-type/FNO1d.py
+++ DeepONet-type/FNO1d.py
@@ -1,6 +1,57 @@
+import torch
 import torch.nn as nn
-from .basics import SpectralConv1d
-from .utils import _get_act
+import torch.nn.functional as F
+
+@torch.jit.script
+def compl_mul1d(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
+    # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
+    res = torch.einsum("bix,iox->box", a, b)
+    return res
+    
+class SpectralConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1):
+        super(SpectralConv1d, self).__init__()
+
+        """
+        1D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        # Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes1 = modes1
+
+        self.scale = (1 / (in_channels*out_channels))
+        self.weights1 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, dtype=torch.cfloat))
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        # Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[2])
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.in_channels, x.size(-1)//2 + 1, device=x.device, dtype=torch.cfloat)
+        out_ft[:, :, :self.modes1] = compl_mul1d(x_ft[:, :, :self.modes1], self.weights1)
+
+        # Return to physical space
+        x = torch.fft.irfftn(out_ft, s=[x.size(-1)], dim=[2])
+        return x
+
+def _get_act(act):
+    if act == 'tanh':
+        func = F.tanh
+    elif act == 'gelu':
+        func = F.gelu
+    elif act == 'relu':
+        func = F.relu_
+    elif act == 'elu':
+        func = F.elu_
+    elif act == 'leaky_relu':
+        func = F.leaky_relu_
+    else:
+        raise ValueError(f'{act} is not supported')
+    return func
 
 # Based on https://github.com/neuraloperator/physics_informed, we got the following code
 class FNO1d(nn.Module):
